9a096ade77f6c01134e3ab601fb83caf6c6b73ba
github user si-n-ya <sin.982.9.s@gmail.com>
2023-09-14 08:35:25 +0900

    axiosを使用しタスク一覧を取得

--- src/resources/ts/pages/tasks/index.tsx
+++ src/resources/ts/pages/tasks/index.tsx
@@ -1,51 +1,46 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
+type Task = {
+  id: number
+  title: string
+  is_done: boolean
+  created_at: Date
+  updated_at: Date
+}
 const TaskPage = () => {
+
+  const [tasks, setTasks] = useState<Task[]>([])
+
+  const getTasks =async () => {
+    // 「<Task[]>(」で取得したデータに型を定義
+    const { data } = await axios.get<Task[]>('api/tasks');
+    setTasks(data)
+  }
+
+  useEffect(() => {
+    getTasks();
+  });
+
   return (
     <>
       <form className="input-form">
         <div className="inner">
-          <input type="text" className="input" placeholder="TODOを入力してください。" value="" />
+          <input type="text" className="input" placeholder="TODOを入力してください。" defaultValue="" />
           <button className="btn is-primary">追加</button>
         </div>
       </form>
       <div className="inner">
         <ul className="task-list">
-          <li>
+          { tasks.map((task) => {
+            <li key={task.id}>
             <label className="checkbox-label">
               <input type="checkbox" className="checkbox-input" />
             </label>
-            <div><span>新しいTODO</span></div>
+            <div><span>{task.title}</span></div>
             <button className="btn is-delete">削除</button>
           </li>
-          <li>
-            <label className="checkbox-label">
-              <input type="checkbox" className="checkbox-input" />
-            </label>
-            <form><input type="text" className="input" value="編集中のTODO" /></form>
-            <button className="btn">更新</button>
-          </li>
-          <li className="done">
-            <label className="checkbox-label">
-              <input type="checkbox" className="checkbox-input" />
-            </label>
-            <div><span>実行したTODO</span></div>
-            <button className="btn is-delete">削除</button>
-          </li>
-          <li>
-            <label className="checkbox-label">
-              <input type="checkbox" className="checkbox-input" />
-            </label>
-            <div><span>ゴミ捨て</span></div>
-            <button className="btn is-delete">削除</button>
-          </li>
-          <li>
-            <label className="checkbox-label">
-              <input type="checkbox" className="checkbox-input" />
-            </label>
-            <div><span>掃除</span></div>
-            <button className="btn is-delete">削除</button>
-          </li>
+          })}
         </ul>
       </div>
     </>
