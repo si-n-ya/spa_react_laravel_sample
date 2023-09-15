8b5e771b6ca537446ecfc284f80ea5532e8c167c
github user si-n-ya <sin.982.9.s@gmail.com>
2023-09-15 09:29:08 +0900

    react-toastifyを導入。TODOチェックのAPI実行時にエラーが発生すればエラーメッセージを表示

--- src/resources/ts/App.tsx
+++ src/resources/ts/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Router from './router';
 import {QueryClient, QueryClientProvider} from 'react-query';
 import { Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   const queryClient = new QueryClient({
@@ -18,6 +20,7 @@ const App = () => {
     <>
       <QueryClientProvider client={queryClient}>
         <Router />
+        <ToastContainer hideProgressBar={true} />
       </QueryClientProvider>
     </>
   )
--- src/resources/ts/queries/TaskQuery.ts
+++ src/resources/ts/queries/TaskQuery.ts
@@ -1,15 +1,21 @@
 import * as api from "../api/TaskAPI"
 import { useQuery, useMutation, useQueryClient } from 'react-query';
+import { toast } from "react-toastify";
 
 const useTasks = () => {
   return useQuery('tasks', () => api.getTasks());  
 }
 
+// TODO 後ほど修正。更新時にgetTasksのタスク全件取得が行われ遅い
 const useUpdateDoneTask = () => {
   const queryClient = useQueryClient();
   return useMutation(api.updateDoneTask, {
     onSuccess: () => {// 成功
       queryClient.invalidateQueries('tasks')// コンポーネントを再描画
+    },
+    onError: () => {
+      // TODO エラーメッセージを表示時は、既に表示されているエラーを削除してから表示
+      toast.error('更新に失敗しました。')
     }
   })
 }
