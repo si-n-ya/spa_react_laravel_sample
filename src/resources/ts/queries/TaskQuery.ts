3ccc789d9b083be7d39d3e70bae18e0ea232eef5
github user si-n-ya <sin.982.9.s@gmail.com>
2023-09-15 09:52:41 +0900

    エラーメッセージを表示時に既存のメッセージは削除

--- src/resources/ts/queries/TaskQuery.ts
+++ src/resources/ts/queries/TaskQuery.ts
@@ -6,16 +6,26 @@ const useTasks = () => {
   return useQuery('tasks', () => api.getTasks());  
 }
 
+// TODO 後ほど方法を変更
+let errorToastId = null;// エラーメッセージのトーストIDを保存する
+
 // TODO 後ほど修正。更新時にgetTasksのタスク全件取得が行われ遅い
 const useUpdateDoneTask = () => {
   const queryClient = useQueryClient();
   return useMutation(api.updateDoneTask, {
     onSuccess: () => {// 成功
       queryClient.invalidateQueries('tasks')// コンポーネントを再描画
+      // 既に表示されているエラーメッセージは削除
+      if (errorToastId) {
+        toast.dismiss(errorToastId);
+      }
     },
     onError: () => {
-      // TODO エラーメッセージを表示時は、既に表示されているエラーを削除してから表示
-      toast.error('更新に失敗しました。')
+      if (errorToastId) {
+        toast.dismiss(errorToastId);
+      }
+      // 新しいエラーメッセージを表示し、そのIDを保存
+      errorToastId = toast.error('更新に失敗しました。');
     }
   })
 }
